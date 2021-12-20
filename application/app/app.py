a375ab325b53455a8dee014bd1fee20283c354b1
Iustin Petrariu <57269343+IustinianPetrariu@users.noreply.github.com>
2021-12-20 20:28:14 +0200

    create custom routes for different category

--- application/app/app.py
+++ application/app/app.py
@@ -43,7 +43,38 @@ def top_response(value):
     return make_response(jsonify(response),200)
     
 
-
+@app.route("/countries/<category>/<value>",methods = ['GET'])
+def response(category,value):
+     # check if the user input is correct 
+    if not category in ['timezone', 'languages','governance']:
+       return make_response(jsonify({"error":"Your input is not correct, information missing.."}), 400)
+    response = []
+    connection = None 
+    try: 
+        #read connection parameters from configuration file
+        params = config()
+        # connect to the PostgreSQL server
+        connection = psycopg2.connect(**params)
+        #create a cursor 
+        cursor = connection.cursor()
+        # execute a statement
+        query = f'''SELECT ID,NAME,{category} FROM COUNTRIES WHERE {category} LIKE '%{value}%' '''
+        print(query,(category,category,value))
+        cursor.execute(query)
+        rows = cursor.fetchall() 
+        #close the cursor
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return make_response(jsonify({"message":error}), 500)
+    finally:
+        if connection is not None:
+            connection.close()
+    # prepare the response for user 
+    for row in rows:
+        dictionary = {'id' : int(row[0]), 'name':row[1], category:row[2]} 
+        response.append(dictionary)
+    return make_response(jsonify(response),200)
 
 
 
